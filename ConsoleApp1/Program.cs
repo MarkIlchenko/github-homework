d380ee1d59655a428ebe76979f417d684a178485
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-09-10 11:45:12 +0300

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    private const double V = 3.3;
+    private static bool isMale = true;
+
+    static void Main()
+    {
+        int myAge = 19;
+        string myName = "Mark Ilchenko";
+        double gpa = V;
+
+        if (isMale)
+        {
+            Console.WriteLine($"Hello I'm {myName}, and I'm {myAge} years old! {gpa}");
+            Console.WriteLine(myName.Substring(1, 5));
+        } else
+        {
+            Console.WriteLine("Add isMale");
+        }
+
+        Console.ReadLine();
+    }
+}
